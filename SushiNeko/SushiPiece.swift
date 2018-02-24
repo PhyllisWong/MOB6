75ba9cd3e04b72ed6edcec4d35a4e0eed8908468
Phyllis Wong <wong.phyllism@gmail.com>
2018-02-24 00:13:57 -0800

    add sushi base

--- SushiNeko/SushiPiece.swift
+++ SushiNeko/SushiPiece.swift
@@ -6,26 +6,6 @@
 //  Copyright © 2018 Phyllis Wong. All rights reserved.
 //
 
-/* import SpriteKit
-
-class SushiPiece: SKSpriteNode {
-    
-    /* Chopsticks objects */
-    var rightChopstick: SKSpriteNode!
-    var leftChopstick: SKSpriteNode!
-    
-    /* You are required to implement this for your subclass to work */
-    override init(texture: SKTexture?, color: UIColor, size: CGSize) {
-        super.init(texture: texture, color: color, size: size)
-    }
-    
-    /* You are required to implement this for your subclass to work */
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
-}
-*/
 
 import SpriteKit
 
